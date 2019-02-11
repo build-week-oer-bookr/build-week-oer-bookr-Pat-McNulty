67820960d6270039330e952c245a4e3f0d1d620d
McNultyPT <mcnultypt@gmail.com>
2019-02-11 12:33:24 -0800

    adding handleChanges and userLogin functions to Login.js to facilitate login

--- oer-bookr/src/components/Login/Login.js
+++ oer-bookr/src/components/Login/Login.js
@@ -9,24 +9,40 @@ class Login extends React.Component {
         };
     }
 
+    userLogin = () => {
+        localStorage.setItem('username', this.state.username);
+        localStorage.setItem('password', this.state.password);
+        window.location.reload();
+    }
+
+    handleChanges = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
     render() {
         return (
             <div>
-                <form>
-                    <input 
-                        type='text'
-                        placeholder='Username'
-                        name='username'
-                        required
-                    />
-                    <input 
-                        type='text'
-                        placeholder='Password'
-                        name='password'
-                        required
-                    />
-                    <button>Login</button>
-                </form>
+                <input 
+                    type='text'
+                    placeholder='Username'
+                    name='username'
+                    value={this.state.username}
+                    onChange={this.handleChanges}
+                    required
+                />
+                <input 
+                    type='text'
+                    placeholder='Password'
+                    name='password'
+                    value={this.state.password}
+                    onChange={this.handleChanges}
+                    required
+                />
+                <button
+                    type='submit'
+                    value='Login'
+                    onClick={this.userLogin}
+                >Login</button>
             </div>
         );
     }
