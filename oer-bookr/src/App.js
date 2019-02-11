a157547a5d7c58c38018b895ba48a597ecb4556d
McNultyPT <mcnultypt@gmail.com>
2019-02-11 10:54:40 -0800

    building basic HomePage component and importing to App.js

--- oer-bookr/src/App.js
+++ oer-bookr/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Login from './components/Login/Login';
+import HomePage from './components/Homepage/HomePage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Login />
+        <HomePage />
       </div>
     );
   }
